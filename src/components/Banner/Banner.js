e67d28b7f5f1eaf3b6d9737102daa3edaf5b8f77
ruhulaminbd93 <ruhulaminbd93@gmail.com>
2021-11-29 23:21:26 +0600

    download resume

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -10,6 +10,18 @@ const Banner = () => {
         Hello ! I'm <span style={{ color: "black" }}>Ruhul Amin</span>
       </h1>
       <h3 style={{ fontWeight: "300" }}>A MERN stack developer</h3>
+
+      <button className="btn btn-dark">
+        {" "}
+        <a
+          style={{ textDecoration: "none", color: "white" }}
+          className="watch-live"
+          href="https://drive.google.com/file/d/14kPP0Y2WNuayYaBDyYXpsXkD52hzrvji/view?usp=sharing"
+          target="_blank"
+        >
+          Download Resume
+        </a>
+      </button>
     </div>
   );
 };
